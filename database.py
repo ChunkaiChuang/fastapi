f9a7fb7053d735cc81fd9bcd5a3cc96c2d794d5f
Eric <ms0355311@gmail.com>
2024-08-23 18:19:18 +0800

    １２４５

--- database.py
+++ database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://todo_trjd_user:wnGdJyL1x9uojDk19YIAHkqNIdCeVv82@dpg-cqumscij1k6c73di53pg-a.singapore-postgres.render.com/todo_trjd"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import models
 from database import engine
 from routers import auth, todos
 from fastapi.staticfiles import StaticFiles
-
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+@app.get("/auth")
+def read_root():
+    return FileResponse ('/templates/login.html')
 
 # 將 static 目錄掛載到 /static 路徑
 app.mount("/static", StaticFiles(directory="static"), name="static")
